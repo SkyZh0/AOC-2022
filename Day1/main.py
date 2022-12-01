ac97f616e184202b3b4ab5c7cfde8d3740468f2a
SkyZh0 <matteo.liagre@gmail.com>
2022-12-01 20:49:18 +0100

    compacted

--- Day1/main.py
+++ Day1/main.py
@@ -1,18 +1,13 @@
 inp = open('Day1/input.txt')
-data = inp.read()
-tolist = data.replace('\n',',').split(',')
-
-final = []
-temp = []
+tolist = inp.read().replace('\n',',').split(',')
+final, temp = [],[]
 for i in tolist:
     if i != '':
         temp.append(int(i))
     else:
         final.append(temp)
         temp = []
-
-sums = [sum(i) for i in final]
-    
+sums = [sum(i) for i in final]    
 maxs = sorted(sums, reverse=True)[:3]
-print('PART-1: ', maxs[0])
-print('PART-2: ', sum(maxs))
+print('PART-1: ', maxs[0], '\nPART-2: ', sum(maxs))
+
