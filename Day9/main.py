d943a5a713e517171d539c883a6d6e6a9913a03e
SkyZh0 <matteo.liagre@gmail.com>
2022-12-09 09:12:09 +0100

    Final Day9

--- Day9/main.py
+++ Day9/main.py
@@ -1,10 +1,5 @@
-"""
-     +y
-      
- -x      +x
-    
-     -y
-"""
+#IMPORT SECTION
+import support
 
 #INPUT HANDLING
 with open("Day9/input.txt") as file:
@@ -12,52 +7,55 @@ with open("Day9/input.txt") as file:
     moves = [elt.split(' ') for elt in moves]
     moves = [[elt[0], int(elt[1])] for elt in moves]
 
-#PART-1
-x,y = 0,0
-movement = 0
-for move in moves:
-    if move[0] == 'U':
-        y += move[1]
-    elif move[0] == 'D':
-        y -= move[1]
-    elif move[0] == 'L':
-        x -= move[1]
-    elif move[0] == 'R':
-        x += move[1]
-    
-    if abs(x) >= 1 and abs(y) > 1:
-        while abs(x) >= 1 and abs(y) > 1:
-            if x > 0 and y > 0:
-                x -= 1
-                y -= 1
-                movement += 1
-            elif x > 0 and y < 0:
-                x -= 1
-                y += 1
-                movement += 1
-            elif x < 0 and y > 0:
-                x += 1
-                y -= 1
-                movement += 1
-            elif x < 0 and y < 0:
-                x += 1
-                y += 1
-                movement += 1
-    if abs(y) >= 2 and abs(x) == 0:
-        while abs(y) >= 2:
-            if y > 0:
-                y -= 1
-                movement += 1
-            else:
-                y += 1
-                movement += 1
-    if abs(x) >= 2 and abs(y) == 0:
-        while abs(x) >= 2:
-            if x > 0:
-                x -= 1
-                movement += 1
-            else:
-                x += 1
-                movement += 1
+#PARAMETERS
+D = {
+    'R': support.Direction4.RIGHT,
+    'U': support.Direction4.UP,
+    'L': support.Direction4.LEFT,
+    'D': support.Direction4.DOWN,
+}
+headpos, tailpos = (0,0), (0,0)
+visited = {tailpos}
 
-print(movement)
+#PART-1
+for movement in moves:
+    move = D[movement[0]]
+    n = movement[1]
+    for k in range(n):
+        headpos = move.apply(*headpos)
+        if abs(headpos[0]-tailpos[0]) >= 2 or abs(headpos[1]-tailpos[1]) >= 2:
+            tailpos = move.opposite.apply(*headpos)
+            visited.add(tailpos)
+print('PART-1: ',len(visited))
+
+#PART-2 -- -- -- -- --
+
+#NEW-PARAMETERS
+pos = [(0,0)]*10
+visited2 = {pos[0]}
+
+#FUNCTIONS
+def mover(headpos,tailpos):
+    hx,hy = headpos
+    tx,ty = tailpos
+    if abs(hy - ty) == 2 and abs(hx - tx) == 2:
+        return ((hx + tx) // 2, (hy + ty) // 2)
+    if abs(hy - ty) == 2:
+        return (hx, (ty + hy) // 2)
+    elif abs(hx - tx) == 2:
+        return ((tx + hx) // 2, hy)
+    else:
+        return tailpos
+
+#COMPUTING
+for movement in moves:
+    move = D[movement[0]]
+    n = movement[1]
+    for k in range(n):
+        pos[0] = move.apply(*pos[0])
+        previous = pos[0]
+        for i in range(1,10):
+            pos[i] = mover(previous, pos[i])
+            previous = pos[i]
+        visited2.add(pos[-1])
+print('PART-2: ',len(visited2))
