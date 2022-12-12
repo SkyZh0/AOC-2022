f0584919df9ded739bdc5be6faab19a904c2cf79
SkyZh0 <matteo.liagre@gmail.com>
2022-12-12 16:27:44 +0100

    Final-Day12

--- Day12/main.py
+++ Day12/main.py
@@ -28,7 +28,7 @@ left = [(0, start)]
 while left:
     cost, pos = heapq.heappop(left)
     if pos == end:
-        print('PART-1: ', cost)
+        print('PART-1: ', cost-2)#339
     elif pos in visited:
         continue
     else:
@@ -40,4 +40,27 @@ while left:
             if ord(prox) - ord(current) <= 1:
                 heapq.heappush(left, (cost+1, cord))
             
-    
+#PART-2
+coords = {}
+for y,line in enumerate(data):
+    for x,c in enumerate(line):
+        coords[(y,x)] = c
+        if c == 'E':
+            end = (y,x)
+visited = set()
+left = [(0,end)]
+while left:
+    cost, pos = heapq.heappop(left)
+    if coords[pos] == 'a':
+        print('PART-2: ',cost-2)#332
+        break
+    elif pos in visited:
+        continue
+    else:
+        visited.add(pos)
+    for cord in support.adjacent_4(*pos):
+        if cord in coords:
+            current = mapped.get(coords[pos], coords[pos])
+            prox = mapped.get(coords[cord], coords[cord])
+            if ord(prox) - ord(current) >= -1:
+                heapq.heappush(left, (cost+1, cord))
