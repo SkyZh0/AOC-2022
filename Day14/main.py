015c516ad5a23dfcf072dd3bdc081fd886ad5df3
SkyZh0 <matteo.liagre@gmail.com>
2022-12-14 08:49:22 +0100

    Final-Day14

--- Day14/main.py
+++ Day14/main.py
@@ -5,7 +5,6 @@ with open('Day14/input.txt') as file:
     data = file.read()
 
 #PART-1
-SAND = (500,0)
 coords = set()
 for line in data.split('\n'):
     points = line.split(' -> ')
@@ -40,5 +39,46 @@ while BOOL:
         if y > maxi:
             print('Part-1: ', count)
             BOOL = False
+            break
+    count += 1
+
+#PART-2
+coords = set()
+for line in data.split('\n'):
+    points = line.split(' -> ')
+    bfx,bfy = support.parse_numbers_comma(points[0])
+    for point in points[1:]:
+        cux,cuy = support.parse_numbers_comma(point)
+        if cux == bfx:
+            for y in range(min(cuy,bfy),max(cuy,bfy)+1):
+                coords.add((cux,y))
+        else:
+            for x in range(min(cux,bfx),max(cux,bfx)+1):
+                coords.add((x,cuy))
+        bfx, bfy = cux, cuy
+maxi = max(y for _, y in coords)
+
+count = 0
+BOOL = True
+while BOOL:
+    x,y = 500, 0
+    while True:
+        if (x,y) in coords:
+            print('PART-2: ', count)
+            BOOL = False
+            break
+        elif (y == maxi +1):
+            coords.add((x,y))
+            break
+        elif (x,y+1) not in coords:
+            y += 1
+        elif (x-1,y+1) not in coords:
+            x -= 1
+            y += 1
+        elif (x+1,y+1) not in coords:
+            x += 1
+            y += 1
+        else:
+            coords.add((x,y))
             break
     count += 1
