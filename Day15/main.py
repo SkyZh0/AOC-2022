61f4c7c241a5279946f3ce2bd14537f6b6d4fc8b
SkyZh0 <matteo.liagre@gmail.com>
2022-12-15 08:47:16 +0100

    Day15 PART-1

---
+++ Day15/main.py
@@ -0,0 +1,43 @@
+#IMPORT SEC
+import re
+import support 
+from typing import NamedTuple
+
+#INPUT HANDLING
+with open('Day15/input.txt') as file:
+    data = file.read().split('\n')
+
+#PART 1
+#USEFULL
+regex = re.compile(
+    r'^Sensor at x=(-?\d+), y=(-?\d+): '
+    r'closest beacon is at x=(-?\d+), y=(-?\d+)$',
+)
+
+class Sensor(NamedTuple):
+    x: int
+    y: int 
+    beacx: int 
+    beacy: int 
+    
+    @property
+    def distCalc(self) -> int:
+        return abs(self.x - self.beacx) + abs(self.y - self.beacy)
+
+#PART-1
+beacs, coords = set(), set()
+y = 2000000
+for line in data:
+    match = regex.match(line)
+    sensor = Sensor(
+        int(match[1]),int(match[2]),int(match[3]),int(match[4])
+    )
+    beacs.add(sensor)
+    dist = sensor.distCalc
+    rght = dist - abs(y - sensor.y)
+    for x in range(sensor.x - rght, sensor.x + rght + 1):
+        coords.add((x, sensor.y))
+print('PART-1: ', len(coords-beacs))
+
+#PART-2
+
