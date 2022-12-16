86afe8e2aef92708ba0ec357361b543b2cf699fd
SkyZh0 <matteo.liagre@gmail.com>
2022-12-16 09:33:28 +0100

    Final-Day16

--- Day16/main.py
+++ Day16/main.py
@@ -56,4 +56,57 @@ while todo:
                 possible - {p},
             ))
 
-print(mini)
+print('PART-1: ',mini)
+
+
+#PART-2
+borders = {}
+flows = {}
+for line in data:
+    _,name,*_ = line.split()
+    compatible_valves = valves.search(line)
+    targets = compatible_valves[1].split(', ')
+    hit = flow.search(line)
+    borders[name] = targets
+    flows[name] = int(hit[1])
+
+rate = frozenset(i for i,j in flows.items() if j)    
+weight = {}
+edges = ['AA',*rate]
+for i,j in itertools.combinations(edges, r=2):
+    todo: collections.deque[tuple[str, ...]]
+    todo = collections.deque([(i,)])
+    while todo:
+        node = todo.popleft()
+        if node[-1] == j:
+            break
+        else:
+            todo.extend((*node,n) for n in borders[node[-1]])
+    weight[(i,j)] = len(node)
+    weight[(j,i)] = len(node)
+
+#TIME TRAVEL
+mini: dict[frozenset[str],int] = {}
+todo: list[tuple[int,int,tuple[str,...], frozenset[str]]]
+todo = [(0,0,('AA',),rate)]
+while todo:
+    point, time, path, possible = todo.pop()
+    route = frozenset(path) - {'AA'}
+    best = mini.setdefault(route, point)
+    mini[route] = max(best,point)
+    
+    for p in possible:
+        ntime = time + weight[(path[-1],p)]
+        if ntime < 26:
+            todo.append((
+                point + (26 - ntime) * flows[p],
+                ntime,
+                path + (p,),
+                possible - {p},
+            ))
+
+print('PART-2:', max(
+    mini[i] + mini[j]
+    for i,j in itertools.combinations(mini, r=2)
+    if not i & j
+))
