3ec673a562007f196ecdf6dce59384b807d34dce
SkyZh0 <matteo.liagre@gmail.com>
2022-12-19 21:27:05 +0100

    Final-Day19

--- Day19/main.py
+++ Day19/main.py
@@ -160,7 +160,119 @@ for i, line in enumerate(data,1):
 print(res)
 
 #PART-2
+def main2(items: list[tuple[str, str, str, str, str]]) -> int:
+    costs: dict[str,dict[str,int]] = collections.defaultdict(dict)
+    for bot_key, n1, cost2, n2, cost3 in items:
+        costs[bot_key][cost2] = int(n1)
+        if n2:
+            costs[bot_key][cost3] = int(n2)
+    cost = Cost(
+        geo_bot_ore=costs['geode']['ore'],
+        geo_bot_obs=costs['geode']['obsidian'],
+        ore_bot_ore=costs['ore']['ore'],
+        cla_bot_ore=costs['clay']['ore'],
+        obs_bot_ore=costs['obsidian']['ore'],
+        obs_bot_cla=costs['obsidian']['clay'],
+    )
+
+    maxo = max(
+        cost.obs_bot_ore,
+        cost.ore_bot_ore,
+        cost.cla_bot_ore,
+        cost.geo_bot_ore,
+    )
+
+    visited = set()
+    best: dict[int,int] = {}
+    todo = collections.deque([(0, 1, 0, 0, 0, 0, 0, 0, 0)])
+    while todo:
+        m, ore_b, cla_b, obs_b, geo_b, ore, cla, obs, geo = todo.popleft()
+        ore = min(maxo * (24-m), ore)
+        cla = min(cost.obs_bot_cla * (24-m), cla)
+        obs = min(cost.geo_bot_obs * (24-m), obs)
+        ore_b = min(ore_b, maxo)
+        cla_b = min(cla_b, cost.obs_bot_cla)
+        geo_b = min(geo_b, cost.geo_bot_obs)
+        
+        tupl = (m,ore_b,cla_b,geo_b,ore,cla,obs,geo)
+        if tupl in visited:
+            continue
+        else:
+            visited.add(tupl)
+        
+        best[m] = max(best.get(m,0),geo)
+        
+        if m == 24:
+                continue
+            
+        if ore >= cost.ore_bot_ore and obs >= cost.geo_bot_obs:
+            todo.apped((
+                m+1,
+                ore_b,
+                cla_b,
+                obs_b,
+                geo_b +1,
+                ore + ore_b - cost.geo_bot_ore,
+                cla + cla_b,
+                obs + obs_b - cost.geo_bot_obs,
+                geo + geo_b,
+            ))
+            continue
+        
+        if ore >= cost.cla_bot_ore and cla >= cost.geo_bot_cla:
+            todo.append((
+                m +1,
+                ore_b,
+                cla_b,
+                obs_b + 1,
+                geo_b,
+                ore + ore_b - cost.obs_bot_ore,
+                cla + cla_b - cost.obs_bot_cla,
+                obs + obs_b,
+                geo + geo_b,
+            ))
+            
+        if ore >= cost.cla_bot_ore:
+            todo.append((
+                m + 1,
+                ore_b,
+                cla_b + 1,
+                obs_b,
+                geo_b,
+                ore + ore_b - cost.cla_bot_ore,
+                cla + cla_b,
+                obs + obs_b,
+                geo + geo_b,
+            ))
+        
+        if ore >= cost.ore_bot_ore:
+            todo((
+                m + 1,
+                ore_b + 1,
+                cla_b,
+                obs_b,
+                geo_b,
+                ore + ore_b - cost.ore_bot_ore,
+                cla + cla_b,
+                obs + obs_b,
+                geo + geo_b,
+            ))
+        
+        todo.append((
+                m + 1,
+                ore_b,
+                cla_b,
+                obs_b,
+                geo_b,
+                ore + ore_b,
+                cla + cla_b,
+                obs + obs_b,
+                geo + geo_b,
+            ))
+        print(best)
+        return(best[32])
+    
 returnval = 1
 for line in data[:3]:
-    ret *= main(regex.findall(line))
+    ret *= main2(regex.findall(line))
 print(returnval)
