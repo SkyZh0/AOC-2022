efdd01aff739da6969a33f213406bbed2ed3426a
SkyZh0 <matteo.liagre@gmail.com>
2022-12-20 16:19:04 +0100

    Day20 Part-1

--- Day20/main.py
+++ Day20/main.py
@@ -1,4 +1,26 @@
 #IMPORT SECTION
+from unittest import mock
+import support
+import collections
+
 #INPUT HANDLING
-#FUNCTIONS AND VARIABLES
+with open('Day20/input.txt') as file:
+    data = file.read()
+
 #PART-1
+OGnums = support.parse_numbers_split(data)
+nums = collections.deque(list(enumerate(OGnums)))
+
+for i, number in enumerate(OGnums):
+    id = nums.index((i, mock.ANY))
+    nums.rotate(-id)
+    nums.rotate(-number)
+    nums.appendleft((i,number))
+    
+id0 = nums.index((mock.ANY, 0))
+print(sum(
+    nums[(id0 + i) % len(nums)][1]
+    for i in (1000,2000,3000)
+    ))
+
+
