79210515a730e0c73b8f66a54c7309fa42dcd022
SkyZh0 <matteo.liagre@gmail.com>
2022-12-20 16:25:08 +0100

    Final-Day20

--- Day20/main.py
+++ Day20/main.py
@@ -18,9 +18,24 @@ for i, number in enumerate(OGnums):
     nums.appendleft((i,number))
     
 id0 = nums.index((mock.ANY, 0))
-print(sum(
+print('PART-1: ',sum(
     nums[(id0 + i) % len(nums)][1]
     for i in (1000,2000,3000)
     ))
 
+#PART-2
+OGnums = support.parse_numbers_split(data)
+OGnums = [i * 811589153 for i in OGnums]
+nums = collections.deque(list(enumerate(OGnums)))
 
+for _ in range(10):
+    for i, number in enumerate(OGnums):
+        id = nums.index((i, mock.ANY))
+        nums.rotate(-id)
+        nums.rotate(-number)
+        nums.appendleft((i,number))
+id0 = nums.index((mock.ANY, 0))
+print('PART-2: ',sum(
+    nums[(id0 + i) % len(nums)][1]
+    for i in (1000,2000,3000)
+    ))
