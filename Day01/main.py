7698eeaf8afb2a597c85b24e6e72f024e1824451
SkyZh0 <matteo.liagre@gmail.com>
2022-12-23 14:48:02 +0100

    Final-Day01

--- Day01/main.py
+++ Day01/main.py
@@ -1,9 +1,12 @@
 import heapq
+
 # Read input file and convert each line to a list of integers
 with open('Day01/input.txt') as inp:
-    final = [list(map(int, line.strip().split(','))) for line in inp]
+    final = [[int(x) for x in line.strip().split(',') if x.isdigit()] for line in inp]
+
 # Find the 3 largest sums
 sums = [sum(i) for i in final]
 maxs = heapq.nlargest(3, sums)
+
 # Output the results
 print('PART-1: ', maxs[0], '\nPART-2: ', sum(maxs))
